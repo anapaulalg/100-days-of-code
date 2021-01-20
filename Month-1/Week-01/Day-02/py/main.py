c1125a79b5eff1910fe98bef7a0a4f26cab8b4ce
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-20 21:19:59 +0100

    Day 02

---
+++ Month-1/Week-01/Day-02/py/main.py
@@ -0,0 +1,9 @@
+# Finding Nemo
+
+sentence = "I am Nemo"
+
+if sentence.find("Nemo") == -1:
+    print("I found Nemo at [the order of the word you find nemo]!")    
+        
+else:
+    print("I can't find Nemo :(")
