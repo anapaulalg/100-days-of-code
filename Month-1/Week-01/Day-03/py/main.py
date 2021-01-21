58b58aef0919e26ba118d81669097bf81ab4c7c5
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-21 23:31:02 +0100

    Day 03 main completed

---
+++ Month-1/Week-01/Day-03/py/main.py
@@ -0,0 +1,20 @@
+# Barbecue Skewers
+
+# skewers = ["--xo--x--ox--", "--xx--x--xx--", "--oo--o--oo--", "--xx--x--ox--", "--xx--x--ox--"]
+# --xo--x--ox-- --xx--x--xx-- --oo--o--oo-- --xx--x--ox-- --xx--x--ox--
+
+skewers = input("What are the skewers? ").split()
+
+def countNonVeg(skewers):
+    numberOfNonVeg = 0
+    for elem in skewers:
+        if 'x' in elem:
+            numberOfNonVeg = numberOfNonVeg + 1
+    return numberOfNonVeg
+
+def countVeg(skewers): 
+    numberOfNonVeg = countNonVeg(skewers)       
+    numberOfVeg = len(skewers) - numberOfNonVeg
+    return numberOfVeg
+
+print("[" + str(countVeg(skewers)) + ", " + str(countNonVeg(skewers)) + "]")
