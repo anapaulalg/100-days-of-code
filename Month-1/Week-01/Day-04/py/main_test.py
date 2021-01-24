06ade23fbeef7f179535b8396f7452a78e688c38
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-24 17:26:41 +0100

    Day 04 completed

--- Month-1/Week-01/Day-04/py/main_test.py
+++ Month-1/Week-01/Day-04/py/main_test.py
@@ -1,16 +1,16 @@
 from main import progressDay
 
 def threeProgressDays():
-    assert countNonVeg([10, 11, 12, 9, 10]) == 3
+    assert progressDay([10, 11, 12, 9, 10]) == 3
 
 def oneProgressDays():
-    assert countNonVeg([6, 5, 4, 3, 2, 9]) == 1
+    assert progressDay([6, 5, 4, 3, 2, 9]) == 1
 
 def zeroProgressDays():
-    assert countNonVeg([9, 9]) == 0
+    assert progressDay([9, 9]) == 0
 
 if __name__ == "__main__":
-    threeProgressDays
-    oneProgressDays
-    zeroProgressDays
+    threeProgressDays()
+    oneProgressDays()
+    zeroProgressDays()
     print('All tests passed')
