8d4eb05fe52be5e60fc158cc8ac369838e0784e4
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-24 16:48:44 +0100

    Day 04 completed

--- Month-1/Week-01/Day-04/py/main.py
+++ Month-1/Week-01/Day-04/py/main.py
@@ -1,6 +1,12 @@
 # Is Johnny Making Progress?
 
-milesperday = [3, 4]
+def progressDay(milesperday):
+    progressDays = 0
+    for i in range(len(milesperday)):
+        if len(milesperday) != i+1 and milesperday[i] < milesperday[(i+1)]:
+            progressDays = progressDays+1       
+    return progressDays        
 
-if milesperday[0] < milesperday[(1)]:
-    print("It is a progress day")
+if __name__ == "__main__":
+    milesperday = [3, 4, 1, 2]
+    print(progressDay(milesperday))
