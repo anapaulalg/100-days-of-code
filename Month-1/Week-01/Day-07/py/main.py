fcf9aa7bd48743cccf944f0154faec221fcdb6fa
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-29 23:57:59 +0100

    Day 07 initial implementation for merge and sort - wip

--- Month-1/Week-01/Day-07/py/main.py
+++ Month-1/Week-01/Day-07/py/main.py
@@ -1,10 +1,23 @@
 # Merge Sorted Array
 
-import numpy as np
-
 nums1 = [1,2,3,0,0,0]
 nums2 = [2,5,6]
 
-nums = nums1 + nums2
+def merge(nums1, nums2):
+    countNums1 = len(nums1)
+    countNums2 = 0
+    for i in range(len(nums1)):
+        print(i)
+        if nums1[i] == 0:
+            for j in range(len(nums2)):
+                print(j)
+                print("Nums1")
+                print(countNums1)
+                print("Nums2")
+                print(countNums2)
+                nums1[i + countNums2] = nums2[j]
+                nums2.pop(j)
+                countNums2 = countNums2 + 1
+                print(nums1)
 
-print(np.sort(nums))
+merge(nums1, nums2)
