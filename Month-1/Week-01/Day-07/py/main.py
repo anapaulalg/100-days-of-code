5c2326bc19c1dfddf8645c43150e250922c707d7
Ana Paula Lasmar Guimarães <ana_paula_lg@hotmail.com>
2021-01-30 23:22:46 +0100

    Day 07 merging and sorting arrays - completed

--- Month-1/Week-01/Day-07/py/main.py
+++ Month-1/Week-01/Day-07/py/main.py
@@ -1,24 +1,24 @@
 # Merge Sorted Array
 
-nums1 = [1,2,3,0,0,0]
-nums2 = [2,5,6]
+
 
 def merge(nums1, nums2):
     countNums1 = len(nums1)
     countNums2 = 0
     for i in range(len(nums1)):
-        print(i)
         if nums1[i] == 0:
             for j in range(len(nums2)):
-                print(j)
-                print("Nums1")
-                print(countNums1)
-                print("Nums2")
-                print(countNums2)
                 nums1[i] = nums2[j]
                 nums2.pop(j)
                 countNums2 = countNums2 + 1
-                print(nums1)
                 break
+    mergedArray = nums1
+    mergedSorted = sorted(mergedArray)
+    print(mergedSorted)
 
-merge(nums1, nums2)
+if __name__ == "__main__":
+    nums1 = [1,2,3,0,0,0]
+    nums2 = [2,5,6]
+    merge(nums1, nums2)
+
+
