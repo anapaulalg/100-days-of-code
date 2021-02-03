e9806af122b6e9451d1bb59b8726aaa2955284af
Ana Paula <ana_paula_lg@hotmail.com>
2021-02-03 23:43:54 +0100

    Day 09 - trapping rain water - wip

--- Month-1/Week-02/Day-09/py/main.py
+++ Month-1/Week-02/Day-09/py/main.py
@@ -2,11 +2,36 @@
 
 height = [4,2,0,3,2,5]
 
-def rainWater(height):
-    for i in height:
-        if (i - 1) and (i + 1) > i:
-            rainWater = (max(height[0:2])**2) - sum(height[i-1:i+2]) - len(height[0:i+2]) - 1
-    print(rainWater)
-    return rainWater
+def findWallIndexes(startIndex, data):
+    walls = []
+    previousValue = 0
+    previousIndex = None
+    for i in range(startIndex, len(data)):
+        if i != 0:
+            previousValue = data[previousIndex]
+        difference = data[i] - previousValue
+        previousIndex = i
+        if difference > 0:
+            if (len(walls) > 1 and walls[-1] == i - 1):
+                walls[-1] = previousIndex
+            else:
+                walls.append(previousIndex)
+        previousIndex = i
+    return walls
 
-rainWater(height)
+def calculateTrappedWater(data, walls):
+    sum = 0
+    previousIndex = None
+    for i in range(len(walls)):
+        if (previousIndex != None and previousIndex != walls[i]):
+            minValue = min([data[walls[i]], data[previousIndex]])
+            for j in range(len(data)):
+                if (j > previousIndex and j < walls[i]):
+                    sum = sum + (minValue - data[j])    
+        previousIndex = walls[i]
+    return sum
+
+walls = findWallIndexes(0, height)
+print(walls)
+result = calculateTrappedWater(height, walls)
+print(result)
