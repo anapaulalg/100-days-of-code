b5f6a7da04bbe02f9913e9b87bcf9a077b0bab33
Ana Paula <ana_paula_lg@hotmail.com>
2021-02-10 23:55:51 +0100

    Day 11 - wip

--- Month-1/Week-02/Day-11/py/main.py
+++ Month-1/Week-02/Day-11/py/main.py
@@ -1,13 +1,21 @@
 # Restore IP Addresses
 
-s = "2600"
+s = "233222777111"
 
-if len(s) < 4:
-    print('This cannot be a valid IP')
-
-ip = int(s[:3])
-if ip <= 255:
-    print(ip)
-else:
-    ipSmall = int(s[:2])
-    print(ipSmall)
+def ipAddresses(s):
+    if len(s) < 4:
+        print('This cannot be a valid IP')
+    else:
+        firstPartIP = int(s[:3])
+        if firstPartIP <= 255:
+            print(str(firstPartIP) + '.')
+        else:
+            firstPartIP = int(s[:2])
+            print(str(firstPartIP) + '.')
+        
+ipAddresses(s)
+firstPartIP = int(s[:3])
+secondPartIP = int(s[3:6])
+thridPartIP = int(s[6:9])
+fourthPartIP = int(s[9:])
+print(str(firstPartIP) + '.' + str(secondPartIP) + '.' + str(thridPartIP) + '.' + str(fourthPartIP))
