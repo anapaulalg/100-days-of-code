026fcd686c908829da27ce294b5306883004d0ad
Ana Paula <ana_paula_lg@hotmail.com>
2021-02-13 23:54:01 +0100

    Day 12 - montains or valleys - wip

--- Month-1/Week-02/Day-12/py/main.py
+++ Month-1/Week-02/Day-12/py/main.py
@@ -1,14 +1,20 @@
 # Mountains or Valleys
 
-landscape = [3, 5, 3]
+landscape = [3, 5, 3, 3, 2]
 
 def landscapeType(landscape):
-    if (landscape[0] < landscape[1] > landscape[2]):
+    ma = max(landscape)
+    maIndex = landscape.index(ma)
+    print(maIndex)
+
+    if (landscape[:maIndex] < ma > landscape[maIndex:]):
         print("montain")
+        
     elif (landscape[0] > landscape[1] < landscape[2]):
         print("valley")
     else:
         print("neither")
+
     return landscapeType
 
 landscapeType(landscape)
