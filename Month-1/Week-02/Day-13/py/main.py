81e109a56eb5cc06a308d958690e6ba6b4f7eb61
Ana Paula <ana_paula_lg@hotmail.com>
2021-02-17 23:48:08 +0100

    Day 13 - packing

---
+++ Month-1/Week-02/Day-13/py/main.py
@@ -0,0 +1,13 @@
+# Need Help With Your Packing?
+
+weighs = [5, 4, 3]
+bags = 2
+
+def CanFit(weighs, bags):
+    if (sum(weighs) / bags) <= 10:
+        print(True)
+    else:
+        print(False)
+    return CanFit
+    
+CanFit(weighs, bags)
