a8f1f8eb2a2cee58dc0ad57fd34423f4b678cd19
Ana Paula <ana_paula_lg@hotmail.com>
2021-02-17 23:54:30 +0100

    Day 13 - tests

---
+++ Month-1/Week-02/Day-13/py/main_test.py
@@ -0,0 +1,16 @@
+from main import CanFit
+
+def testOne():
+    assert CanFit([1, 4, 1], 1) == True
+
+def testTwo():
+    assert CanFit([9, 7, 20], 2) == False
+
+def testThree():
+    assert CanFit([2, 2, 12], 2) == False
+
+if __name__ == "__main__":
+    testOne()
+    testTwo()
+    testThree()
+    print("All tests passed")
