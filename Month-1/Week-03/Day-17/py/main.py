897418ab25a1438abadf3b3ecd3e4aeaf061b457
Ana Paula <ana_paula_lg@hotmail.com>
2021-03-03 22:48:41 +0100

    Day 17 completed

--- Month-1/Week-03/Day-17/py/main.py
+++ Month-1/Week-03/Day-17/py/main.py
@@ -1,18 +1,16 @@
 # Prison Break
 
-prison = [1, 1, 0, 0, 0, 1, 0]
-
 def prisonBreak(prison):
     n = 0
     for i in range(0, len(prison)):
-        print(i)
         if i == 0 and prison[i] == 0:
             n = 0
             break
         elif prison[i] == 1:            
             n = n + 1
             prison = [0 if i==1 else 1 for i in prison]
-            print(prison)
     return n
 
-print(prisonBreak(prison))
+if __name__ == "__main__":
+    prison = [1, 1, 0, 0, 0, 1, 0]
+    print(prisonBreak(prison))
